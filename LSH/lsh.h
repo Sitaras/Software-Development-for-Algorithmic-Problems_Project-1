b8981c287f5ab62e3818340332265c4493f00251
Ioannis Kapetangeorgis <giannhskp168@gmail.com>
2021-10-19 21:09:32 +0300

    Renamed LSH

---
+++ LSH/lsh.h
@@ -0,0 +1,9 @@
+#ifndef LSH_H
+#define LSH_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#endif
--- Lhs/lhs.h
+++
@@ -1,9 +0,0 @@
-#ifndef LHS_H
-#define LHS_H
-
-
-#include <stdio.h>
-#include <stdlib.h>
-
-
-#endif
