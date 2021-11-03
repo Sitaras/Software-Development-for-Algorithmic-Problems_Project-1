066299c1988b5e600d19ee7ee1fb3c681f4be769
giannhskp <giannhskp168@gmail.com>
2021-11-03 12:33:16 +0200

    Added binary search on kmeans++

--- Clustering/kmeansPlusPlus.c
+++ Clustering/kmeansPlusPlus.c
@@ -16,6 +16,34 @@
 
 extern int numOfVecs;
 
+int binarySearchKmeans( double key, double data[], const int len )
+{
+    int low  = 0;
+    int high = len-1;
+
+    while( low <= high)
+    {
+        // To convert to Javascript:
+        // var mid = low + ((high - low) / 2) | 0;
+        int mid = low + ((high - low) / 2);
+
+        /**/ if (data[mid] < key) low  = mid + 1;
+        else if (data[mid] > key) high = mid - 1;
+        else return                      mid + 1;
+    }
+
+    if( high < 0 )
+        return 0;   // key < data[0]
+    else
+    if( low > (len-1))
+        return len; // key >= data[len-1]
+    else
+        return (low < high)
+            ? low  + 1
+            : high + 1;
+}
+
+
 
 void kmeansplusplus(Vector* vecs,int numOfClusters,Vector* clusters,double *props){
   int t=1;
@@ -59,16 +87,15 @@ void kmeansplusplus(Vector* vecs,int numOfClusters,Vector* clusters,double *prop
     }
 
     double x=uniform_distribution(0,p[numOfVecs-t-1]);
-
-    for(int r = 1; r<numOfVecs; r++){
-      if(existsInArray(selectedCluster,r,numOfClusters)){
-        continue;
-      }
-      if(p[r-1]<x && x<=p[r]){
-        selectedCluster[t] = index[r];
-        clusters[t++] = copyVector(vecs[index[r]]);
-        break;
-      }
-    }
+    // for(int r = 1; r<numOfVecs-t; r++){
+    //   if(p[r-1]<x && x<=p[r]){
+    //     selectedCluster[t] = index[r];
+    //     clusters[t++] = copyVector(vecs[index[r]]);
+    //     break;
+    //   }
+    // }
+    int bin_index = binarySearchKmeans(x,p,numOfVecs-t);
+    selectedCluster[t] = index[bin_index];
+    clusters[t++] = copyVector(vecs[index[bin_index]]);
   }
 }
